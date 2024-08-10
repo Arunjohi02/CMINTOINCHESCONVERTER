51d57ef09e31b11e2f35ce668ab2da21f1265b91
Arun Johi A <161052550+Arunjohi02@users.noreply.github.com>
2024-08-10 16:18:24 +0530

    Add files via upload

---
+++ JS/js.js
@@ -0,0 +1,7 @@
+let user_value=document.getElementById("user_values");
+let display=document.getElementById("display");
+function Change_button()
+{
+    Container=user_value.value/2.54;
+    display.innerHTML=Container;
+}
